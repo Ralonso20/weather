e255f77f296fd1cbab6db3469160f9f3e3eacec7
Rodrigo Alonso <rodrigoalonso2000@gmail.com>
2022-12-02 02:12:01 -0300

    correccion estilos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
-
-
+import { useRouter } from "next/router";
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {router.push("/start")}, [])
   return <Component {...pageProps} />
 }
 
